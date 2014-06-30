ef837ca4b297387fbc0ba01a359965320b7d8f7d
Eric Matthews <ematthews@verisign.com>
2014-06-30 13:20:13 -0400

    Revert external resources to relative URL

--- phplib/header.php
+++ phplib/header.php
@@ -2,10 +2,10 @@
 <html lang="en">
 <head>
 <title><?=$page_title ?></title>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.0/jquery-ui.min.js"></script>
-<script src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
-<link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.0/jquery-ui.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
+<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
 <script src="<?=$ROOT_URL;?>/assets/wysihtml5-0.3.0.js"></script>
 <script src="<?=$ROOT_URL;?>/assets/bootstrap-wysihtml5.js"></script>
 <link rel="stylesheet" type="text/css" href="<?=$ROOT_URL;?>/assets/bootstrap-wysihtml5.css"></link>
