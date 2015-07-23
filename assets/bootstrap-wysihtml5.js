f8c5d34204899655cedecfa1966825e93a55c699
Russ Taylor <russt@etsy.com>
2015-07-23 19:44:59 +0000

    Removing import of nonexistent stylesheet.

--- assets/bootstrap-wysihtml5.js
+++ assets/bootstrap-wysihtml5.js
@@ -450,7 +450,7 @@
                 "pre": 1
             }
         },
-        stylesheets: ["./lib/css/wysiwyg-color.css"], // (path_to_project/lib/css/wysiwyg-color.css)
+        stylesheets: [],
         locale: "en"
     };
 
